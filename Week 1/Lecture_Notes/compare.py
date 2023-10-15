b374dbbdf28defe082ff0a2d8e8446d0b9ccf08e
Via <alihantasyurek@gmail.com>
2023-10-15 17:46:18 +0300

    refactor

--- Week 1/Lecture_Notes/compare.py
+++ Week 1/Lecture_Notes/compare.py
@@ -1,12 +1,12 @@
+#------------------------------------------------------------------------------------------------
 x = int(input("What's X? "))
 y = int(input("What's y? "))
-
 #most optimized version
 if x != y:
     print("x is not equal to y")
 else:
     print("x is equal to y")
-
+#------------------------------------------------------------------------------------------------
 # optimizing the flow chart
 #    if x < y:
 #        print("x is less than y")
@@ -16,9 +16,10 @@ else:
 
 #    else:
 #        print("x is equal to y")
-
+#------------------------------------------------------------------------------------------------
 # optimizing further
 #    if x < y or x > y:
 #        print("x is not equal to y")
 #    else:
 #        print("x is equal to y")
+#------------------------------------------------------------------------------------------------
